0edc259efaa6cf58c5ece591cceaed0aebc931f0
d0dders <dodders@outlook.com>
2021-02-28 20:21:39 +0000

    Added printQuote function and star ratings

--- js/script.js
+++ js/script.js
@@ -15,49 +15,57 @@ const quotes = [
     quote: "May the Force be with you.",
     source: "General Dodonna",
     citation: "Star Wars",
-    year: "1977"
+    year: "1977",
+    rating: 5
   },
   {
     quote: "There's no place like home.",
     source: "Dorothy",
-    citation: "The Wizard of Oz,",
-    year: "1939"
+    citation: "The Wizard of Oz",
+    year: "1939",
+    rating: 4
   },
   {
     quote: "I'm the king of the world!",
     source: "Jack Dawson",
     citation: "Titanic",
-    year: "1997"
+    year: "1997",
+    rating: 4
   },
   {
     quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
     source: "John Keating",
     citation: "Dead Poets Society",
-    year: "1989"
+    year: "1989",
+    rating: 4
   },
   {
     quote: "You're gonna need a bigger boat.",
     source: "Chief Brody",
     citation: "Jaws",
-    year: "1975"
+    year: "1975",
+    rating: 4
   },
   {
     quote: "It's alive! It's alive!",
     source: "Henry Frankenstein",
     citation: "Frankenstein",
-    year: "1931"
+    year: "1931",
+    rating: 3
   },
   {
     quote: "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
     source: "Forest Gump",
     citation: "Forest Gump",
-    year: "1994"
+    year: "1994",
+    rating: 5
   },
   {
     quote: "I'll be back.",
     source: "T-800",
     citation: "The Terminator",
-    year: "1984"
+    year: "1984",
+    rating: 4
   }
 ]
 
@@ -74,7 +82,27 @@ function getRandomQuote() {
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  const quote = getRandomQuote();
+  let html = '';
+  html += `<p class="quote">${quote.quote}</p>`;
+  html += `<p class="source">${quote.source}`;
+  if (quote.citation) {
+    html += `<span class="citation">${quote.citation}</span>`;
+    html += `<span class="year">${quote.year}</span>`;
+  }
+  if (!isNaN(quote.rating)) {
+    html += `<span class="rating">`;
+    for(let i = 1; i <= quote.rating; i++) {
+      html += `&#9733;`; 
+    }
+    html += `</span>`;
+  }
+  html += `</p>`;
 
+
+  document.getElementById('quote-box').innerHTML = html; 
+}
 
 
 /***
